fbeabf72096990ac667923082ca963993d5a45c7
pedro <mahcarponel12@gmail.com>
2022-01-30 20:27:15 +0100

    29JUL

--- src/Calculadora.php
+++ src/Calculadora.php
@@ -10,7 +10,7 @@ class calculadora
         $this->num1 = $a1;
         $this->num2 = $a2;
     }
-    public function dividir()
+    public function di()
     {
         return $this->num1 / $this->num2;
     }
